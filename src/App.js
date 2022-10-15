b5ff42bbecf16068f0c18e94d5ad6db5329b0e58
Juhi Bhardwaj <nitibhardwaj20@gmail.com>
2022-10-15 10:15:27 +0000

    Throttling and Debounce Example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./styles.css";
 import SetTimeoutExample from "./Components/SetTimeoutExample";
 import IncDecfunction from "./Components/IncreamentCounter";
-import TodoList from "./Components/TodoExample/TodoList"
+import TodoList from "./Components/TodoExample/TodoList";
+import DebounceThrottling from "./Components/DebounceThrottling";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <IncDecfunction />
-       <SetTimeoutExample />
-      <br/>
-      <TodoList/>
+      <SetTimeoutExample />
+      <br />
+      <TodoList />
+      <DebounceThrottling />
     </div>
   );
 }
